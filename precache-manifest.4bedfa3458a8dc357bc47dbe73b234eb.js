33575df447685ec184a0891aa24dd7ac1564bab2
Matias Rivas <matiutu98@gmail.com>
2020-08-26 14:29:15 -0300

    Updates

---
+++ precache-manifest.4bedfa3458a8dc357bc47dbe73b234eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aeb24579aa818312ee7a40f30fae58",
+    "url": "/matea-entrega2-deploy/index.html"
+  },
+  {
+    "revision": "a946fe91ab52916c70a8",
+    "url": "/matea-entrega2-deploy/static/css/main.27db1981.chunk.css"
+  },
+  {
+    "revision": "1efb7017e730d5aa0f55",
+    "url": "/matea-entrega2-deploy/static/js/2.b84ab868.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/matea-entrega2-deploy/static/js/2.b84ab868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a946fe91ab52916c70a8",
+    "url": "/matea-entrega2-deploy/static/js/main.73c7fb1c.chunk.js"
+  },
+  {
+    "revision": "2554354ba0164cacbb67",
+    "url": "/matea-entrega2-deploy/static/js/runtime-main.2919ea15.js"
+  }
+]);
